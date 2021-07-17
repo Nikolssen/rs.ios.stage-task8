ddb48d4d3df55a08dcff203fa45faf06c88d1477
Nikolssen <ibudovichivan3@gmail.com>
2021-07-17 15:50:18 +0300

    Add Swift-Objc interoperability

---
+++ rs.ios.stage-task8/Controller/DrawingController.swift
@@ -0,0 +1,29 @@
+//
+//  DrawingController.swift
+//  rs.ios.stage-task8
+//
+//  Created by Admin on 17.07.2021.
+//
+
+import UIKit
+
+class DrawingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
